ef316f7e623a8137f317eb8dc8e71491b207b68c
ProgrammerTon <pattarapol.nanetra@gmail.com>
2024-10-27 12:33:19 +0700

    show parallel time in output_1_time

--- conv_parallel.c
+++ conv_parallel.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
 
-int main(){
+int main() {
     // ---- input and malloc A, F ----
     int NA, NF;
     scanf("%d %d", &NA, &NF);
     int *A = malloc(sizeof(int) * NA);
     int *F = malloc(sizeof(int) * NF);
-    for(int i = 0; i < NA; i++){
+    for (int i = 0; i < NA; i++) {
         scanf("%d", &A[i]);
     }
-    for(int i = 0; i < NF; i++){
+    for (int i = 0; i < NF; i++) {
         scanf("%d", &F[i]);
     }
-    // ---- end input and malloc----
+    // ---- end input and malloc ----
 
-    int  size_answer = NA - NF + 1;
-    int *answer = (int*)malloc(sizeof(int) * size_answer);
+    int size_answer = NA - NF + 1;
+    int *answer = (int *)malloc(sizeof(int) * size_answer);
 
     double itime, ftime, exec_time;
     itime = omp_get_wtime();
-    
+
     omp_set_num_threads(4);
     #pragma omp parallel for
     for (int i = 0; i < size_answer; i++) {
@@ -29,19 +29,22 @@ int main(){
         for (int j = 0; j < NF; j++) {
             answer[i] += A[i + j] * F[NF - j - 1];
         }
-        
     }
 
     ftime = omp_get_wtime();
     exec_time = ftime - itime;
-    //printf("\n\nTime taken is %f\n\n", exec_time);
-    
+
     for (int i = 0; i < size_answer; i++) {
         printf("%d", answer[i]);
-        if(i!=size_answer-1){
+        if (i != size_answer - 1) {
             printf("\n");
         }
     }
+
+    // Write execution time to output_1_time.txt
+    FILE *time_file = fopen("output_1_time.txt", "w");
+    fprintf(time_file, "Time taken is %f seconds\n", exec_time);
+    fclose(time_file);
 
     // ---- free memory ----
     free(F);
@@ -50,22 +53,3 @@ int main(){
     // ---- end free ----
     return 0;
 }
-
-/*
-8 5
-7
-5
-3
-2
-0
-4
-2
-5
-1
-1
-2
-2
-1
-*/
-
-//printf("thread number use : %d\n", omp_get_thread_num());
